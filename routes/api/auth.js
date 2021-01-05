b19f3a2c4fa9ed7be6569a06dfcd76c36304b460
sahinyasasi <sahinya.slc@gmail.com>
2021-01-05 15:04:57 +0530

    jwt verification and user displaying

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 const router = express.Router();
 //path  api/auth
 //desc test route
 //access  public
-router.get("/", (req, res) => {
-  res.send("auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).send("server error");
+  }
 });
 module.exports = router;
