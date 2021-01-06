a25d5b894f1c50d6d0b37ff8d84267ec0a967cbf
sahinyasasi <sahinya.slc@gmail.com>
2021-01-06 13:31:42 +0530

    to remove experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -191,4 +191,24 @@ router.put(
   }
 );
 
+//path delete api/profile/experience
+//desc to delete experience
+//access  private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    });
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
