2fef5fbffeeba9f28d56f4941afbdfd3cd9e19d4
sahinyasasi <sahinya.slc@gmail.com>
2021-01-08 09:49:00 +0530

    navbar and landing pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
 import "./App.css";
 
 const App = () => (
   <Fragment>
-    <h1>app</h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 );
 
