2b329772d1cfa7f7305d9c69ca04a1719ddc5e1d
sahinyasasi <sahinya.slc@gmail.com>
2021-01-20 09:50:26 +0530

    profile form component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Register from "./components/auth/Register";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 //Redux
@@ -35,6 +36,11 @@ const App = () => {
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
