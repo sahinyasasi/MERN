4159c6db4f0894822036323a9a1cd6bb89ab0e2d
sahinyasasi <sahinya.slc@gmail.com>
2021-01-22 09:10:09 +0530

    github repo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -285,4 +285,32 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   }
 });
 
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}
+     `,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: "No Github Profile Found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: "No Github profile found" });
+  }
+});
+
 module.exports = router;
