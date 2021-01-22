44059b6ab7c343224f668d3ab8e8f753e10fc352
sahinyasasi <sahinya.slc@gmail.com>
2021-01-22 10:55:28 +0530

    add likes and remove elikes

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKE } from "./types";
 import { setAlert } from "./alert";
 //GET POSTS
 export const getPosts = () => async (dispatch) => {
@@ -8,6 +8,38 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statustext, status: err.response.status },
+    });
+  }
+};
+
+//add likes
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+    dispatch({
+      type: UPDATE_LIKE,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statustext, status: err.response.status },
+    });
+  }
+};
+
+//remove likes
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/unlike/${postId}`);
+    dispatch({
+      type: UPDATE_LIKE,
+      payload: { postId, likes: res.data },
     });
   } catch (err) {
     dispatch({
